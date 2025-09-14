62b2b1483a88966809fa7e9431a94a61883626c0
Mustafa adin <111022873+mustaf4din@users.noreply.github.com>
2025-09-14 20:12:18 +0100

    Update 0-databaseconnection.py

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -3,3 +3,4 @@
 #    Write a class custom context manager DatabaseConnection using the __enter__ and the __exit__ methods
 
 #    Use the context manager with the with statement to be able to perform the query SELECT * FROM users. Print the results from the query.
+#["__init__", "return"]
