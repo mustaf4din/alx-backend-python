7869d4c634b4b2906ad9b3db4cf6cea075719f8d
Mustafa adin <111022873+mustaf4din@users.noreply.github.com>
2025-09-14 20:13:23 +0100

    Create 1-execute.py

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,5 @@
+#nstructions:
+
+ #   Implement a class based custom context manager ExecuteQuery that takes the query: ”SELECT * FROM users WHERE age > ?” and the parameter 25 and returns the result of the query
+
+ #   Ensure to use the__enter__() and the __exit__() methods
